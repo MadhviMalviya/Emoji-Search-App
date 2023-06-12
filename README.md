d873f29e3ba5dc8c8c1af47929ccb80db9c9ca49
Madhvi Malviya <madhvimalviya1@gmail.com>
2023-06-12 23:37:15 +0530

    emoji app created

--- README.md
+++ README.md
@@ -68,3 +68,14 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+
+
+
+
+
+
+
+<!-- Create an emoji search app, where you can type and search for any emoji from the given list of emojis -->
--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import React, { useState } from 'react';
+
+
+const emojis = [
+  "😀", "😃", "😄", "😁", "😆", "😅", "🤣", "😂", "😊", "😇",
+  "🙃", "😉", "😌", "🥰", "😍", "🤩", "😘", "😗", "😚", "😋",
+  "😛", "😝", "😜", "🤪", "🤨", "🧐", "🤓", "😎", "🥳", "😏",
+  "😒", "😞", "😔", "😟", "😕", "🙁", "☹️", "😣", "😖", "😫"
+];
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  function handleSearch (e) {
+    setSearchTerm(e.target.value);
+  }
+
+  const filteredEmojis = emojis.filter(emoji => {
+    return emoji.includes(searchTerm);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Emoji Search App</h1>
+      
+      <input
+        type="text"
+        placeholder="Search for an emoji..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <div className="emoji-list">
+        {filteredEmojis.map(emoji => (
+          <span key={emoji} className="emoji">{emoji}</span>
+        ))}
+      </div>
     </div>
   );
 }
